c0253eca5fbe2e061e823349e133a8620c81b648
nickcoding <50139127+nickyoyo@users.noreply.github.com>
2021-07-11 00:42:49 +0800

    2021.7.11 version-mail

--- config/mail.php
+++ config/mail.php
@@ -14,6 +14,7 @@ return [
     */
 
     'default' => env('MAIL_MAILER', 'smtp'),
+    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
 
     /*
     |--------------------------------------------------------------------------
