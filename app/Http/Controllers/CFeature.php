1c004b49a4b2b39edb7a2cd1381c56bd63f27c6b
nickcoding <50139127+nickyoyo@users.noreply.github.com>
2021-07-16 00:39:01 +0800

    2021.7.16 version-CFeature

---
+++ app/Http/Controllers/CFeature.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\adatas;
+use App\Models\adatafinishes;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Session;
+
+class CFeature extends Controller
+{
+    
+}
---
+++ resources/views/CFeature/CFeature.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master')
+@extends('layouts.list')
+@extends('layouts.notlogin')
+
+@section('content')
+    <body class="antialiased">
+      <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+                <div class="topic">
+                    <h1>功能C</h1>         
+               </div>
+               <dd><a href="CFeature1" target='_parent'>功能C-1</a>
+                <a href="CFeature2" target='_parent'>功能C-2</a></dd>        
+               
+        </div>
+    </body>
+@endsection
